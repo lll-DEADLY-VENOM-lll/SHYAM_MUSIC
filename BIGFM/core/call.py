d1f1ffa940cb0ae5804cbb88f82e5f7aef905412
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-01-06 13:55:47 +0530

    0.3 version

--- BIGFM/core/call.py
+++ BIGFM/core/call.py
@@ -1,6 +1,4 @@
-import asyncio
 from typing import Union
-
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 from pytgcalls.types import (
@@ -11,23 +9,17 @@ from pytgcalls.types import (
     Update,
 )
 from pytgcalls.types.stream import StreamAudioEnded
-from pytgcalls.exceptions import (
-    AlreadyJoined,
-    NoActiveGroupCall,
-    TelegramServerError,
-)
+from pytgcalls.exceptions import AlreadyJoined, NoActiveGroupCall, TelegramServerError
 
 import config
 from BIGFM import LOGGER
 from BIGFM.misc import db
 from BIGFM.utils.database import (
     add_active_chat,
-    add_active_video_chat,
     get_lang,
     group_assistant,
     music_on,
     remove_active_chat,
-    remove_active_video_chat,
 )
 from BIGFM.utils.exceptions import AssistantErr
 from BIGFM.utils.stream.autoclear import auto_clean
@@ -36,7 +28,6 @@ from strings import get_string
 
 async def _clear_(chat_id):
     db[chat_id] = []
-    await remove_active_video_chat(chat_id)
     await remove_active_chat(chat_id)
 
 
@@ -44,47 +35,28 @@ class Call:
     def __init__(self):
         self.clients = []
 
-        for i, string in enumerate(
-            [config.STRING1, config.STRING2, config.STRING3, config.STRING4, config.STRING5],
-            start=1,
-        ):
+        strings = [
+            config.STRING1,
+            config.STRING2,
+            config.STRING3,
+            config.STRING4,
+            config.STRING5,
+        ]
+
+        for i, string in enumerate(strings, start=1):
             if string and str(string).strip() not in ["None", ""]:
-                userbot = Client(
-                    name=f"AviaxAss{i}",
+                user = Client(
+                    f"AviaxAss{i}",
                     api_id=config.API_ID,
                     api_hash=config.API_HASH,
                     session_string=string.strip(),
                 )
-                call = PyTgCalls(userbot)
-                self.clients.append(call)
+                self.clients.append(PyTgCalls(user))
 
-    async def pause_stream(self, chat_id: int):
+    async def join_call(self, chat_id, original_chat_id, link, video=False):
         assistant = await group_assistant(self, chat_id)
-        await assistant.pause_stream(chat_id)
-
-    async def resume_stream(self, chat_id: int):
-        assistant = await group_assistant(self, chat_id)
-        await assistant.resume_stream(chat_id)
-
-    async def stop_stream(self, chat_id: int):
-        assistant = await group_assistant(self, chat_id)
-        try:
-            await _clear_(chat_id)
-            await assistant.leave_group_call(chat_id)
-        except:
-            pass
-
-    async def join_call(
-        self,
-        chat_id: int,
-        original_chat_id: int,
-        link,
-        video: Union[bool, str] = None,
-        image: Union[bool, str] = None,
-    ):
-        assistant = await group_assistant(self, chat_id)
-        language = await get_lang(chat_id)
-        _ = get_string(language)
+        lang = await get_lang(chat_id)
+        _ = get_string(lang)
 
         stream = (
             AudioVideoPiped(link, HighQualityAudio(), MediumQualityVideo())
@@ -110,36 +82,31 @@ class Call:
             await _clear_(chat_id)
             return await client.leave_group_call(chat_id)
 
-        try:
-            popped = queue.pop(0)
-            await auto_clean(popped)
+        popped = queue.pop(0)
+        await auto_clean(popped)
 
-            next_file = queue[0]["file"]
-            is_video = str(queue[0]["streamtype"]) == "video"
+        next_file = queue[0]["file"]
+        is_video = str(queue[0]["streamtype"]) == "video"
 
-            stream = (
-                AudioVideoPiped(next_file, HighQualityAudio(), MediumQualityVideo())
-                if is_video
-                else AudioPiped(next_file, HighQualityAudio())
-            )
+        stream = (
+            AudioVideoPiped(next_file, HighQualityAudio(), MediumQualityVideo())
+            if is_video
+            else AudioPiped(next_file, HighQualityAudio())
+        )
 
-            await client.change_stream(chat_id, stream)
-        except:
-            pass
+        await client.change_stream(chat_id, stream)
 
     async def start(self):
         LOGGER(__name__).info("Starting Assistant Clients...")
         for client in self.clients:
             await client.start()
-            self._register_decorators(client)
+            self._decorators(client)
 
-    def _register_decorators(self, client: PyTgCalls):
+    def _decorators(self, client):
         @client.on_stream_end()
-        async def stream_end_handler(_, update: Update):
+        async def _(c, update: Update):
             if isinstance(update, StreamAudioEnded):
                 await self.change_stream(client, update.chat_id)
 
 
 Aviax = Call()
-
-
